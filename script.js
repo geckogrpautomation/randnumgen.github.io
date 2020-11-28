c8327723528cd51f8af99686efd052c754cd4b02
geckogrpautomation <73622298+geckogrpautomation@users.noreply.github.com>
2020-11-28 20:23:04 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 
-
 //Get requried check box objects////////////////////////////////////////
 var lCaseCheckBox = document.getElementById("lCaseCheck");
 var uCaseCheckBox = document.getElementById("uCaseCheck");
 var numCheckBox = document.getElementById("numericalCheck");
 var specialCheckBox = document.getElementById("specialCheck");
 
-
+//Create charset tags
+var lCaseCharLib = "abcdefghijklmnopqrstuvwxyz"
+var uCaseCharLib ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numCharLib ="0123456789"
+var specCharLib =" !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
 //Get required input box objects
 var lCaseNum = document.getElementById("lCaseCheckNum");
@@ -14,13 +17,8 @@ var uCaseNum = document.getElementById("UCaseCheckNum");
 var numCaseNum = document.getElementById("numCheckNum");
 var specialNum = document.getElementById("specialCheckNum");
 
-
-
-
 //Create required events
 document.getElementById("generate").addEventListener("click", initGenPassword);
-
-
 
 
 // Write password to the #password input
@@ -30,7 +28,6 @@ function initGenPassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
 
 
 //Function to build part of the user password based upon length and type of char set                          
@@ -45,11 +42,9 @@ function buildPass(passNumLength, charset) {
   }
   
   
-
-
 //Construct string shuffle 
 function shuffle(str) {
-  
+
   var x = str.split("");
   
   for(var i = x.length - 1; i > 0; i--) {
@@ -66,13 +61,13 @@ function shuffle(str) {
 
 //Funtion to generate password based upon desire char set and desired password length. char set assignment split randmonly across 4 char set values
 function generateRandomnPassword() {
-  
+
 // Declare user input OK flags and force to false
 var lCaseInputOK =  false;
 var uCaseInputOK = false ;
 var numInputOK= false;
 var specialInputOK = false;
-var passlengthOK = false;
+
 
 
 
@@ -84,109 +79,112 @@ var specialInt = parseInt(specialNum.value);
 
 
 // Check user inputs are OK
-if (lCaseCheckBox.checked === true && (lCaseNumInt > 0 || lCaseNumInt !== undefined)){
-  lCaseInputOK = true;
+if (lCaseCheckBox.checked && (lCaseNumInt > 0 || lCaseNumInt !== undefined)){
+lCaseInputOK = true;
 } else if (lCaseCheckBox.checked === false) {
-  lCaseInputOK = true;
-  }
-  
-if (uCaseCheckBox.checked === true && (uCaseNumInt > 0 || uCaseNumInt !== undefined)){
-  uCaseInputOK = true;
+lCaseInputOK = true;
+}
+
+if (uCaseCheckBox.checked && (uCaseNumInt > 0 || uCaseNumInt !== undefined)){
+uCaseInputOK = true;
 } else if (uCaseCheckBox.checked === false) {
-  uCaseInputOK = true;
-  }
+uCaseInputOK = true;
+}
 
-if (numCheckBox.checked === true &&  (numInt > 0 || numInt !== undefined)){
-  numInputOK = true;
+if (numCheckBox.checked &&  (numInt > 0 || numInt !== undefined)){
+numInputOK = true;
 } else if (numCheckBox.checked === false) {
-  numInputOK = true;
-  }
+numInputOK = true;
+}
 
-if (specialCheckBox.checked === true && (specialInt > 0 || specialInt !== undefined)){
-  specialInputOK = true;
+if (specialCheckBox.checked && (specialInt > 0 || specialInt !== undefined)){
+specialInputOK = true;
 } else if (specialCheckBox.checked === false) {
-  specialInputOK = true;
-  }
+specialInputOK = true;
+}
 
 //If inputs are OK
-if (lCaseInputOK ===  true && uCaseInputOK === true && numInputOK === true && specialInputOK === true) {
-  
-  //Define password length from user inputs if checkboxes enabled for that setpoint
-  var passLength = 0;
-  var tempCalc = 0;
-  
-//Add to find total password length
-  if (lCaseCheckBox.checked === true){
-    tempCalc = lCaseNumInt;
-  }
+if (lCaseInputOK  && uCaseInputOK  && numInputOK  && specialInputOK ) {
 
-  if (uCaseCheckBox.checked === true){
-    tempCalc = uCaseNumInt+ tempCalc;
-  }
-
-  if (numCheckBox.checked  === true){
-    tempCalc = numInt+ tempCalc;
-  }
-
-  if (specialCheckBox.checked === true){
-    tempCalc= specialInt+ tempCalc;
-  }
+//Define password length from user inputs if checkboxes enabled for that setpoint
+var passLength = 0;
+var tempCalc = 0;
 
 //Add to find total password length
-  passLength = tempCalc;
- 
-  if (passLength >= 8 && passLength <= 128){   
-                           
-                                
-    // Declare blank strings for user char set and the apssword output
-      var generatedOutput= "";
-      var lCasePass = "";
-      var uCasePass = "";
-      var numPass = "";
-      var specialPass = "";
-              
-      //Concatenate allowable characters based upon user feedback from confirmation input
-      if (lCaseCheckBox.checked === true && lCaseNumInt > 0){
-        lCasePass = buildPass(lCaseNumInt , "abcdefghijklmnopqrstuvwxyz");   
-        console.log("lower case char count "  + lCaseNumInt  )  
-        console.log("lower password "  + lCasePass);             
-      }
+if (lCaseCheckBox.checked ){
+  tempCalc = lCaseNumInt;
+}
 
-      if (uCaseCheckBox.checked === true && uCaseNumInt > 0  ){
-        uCasePass = buildPass(uCaseNumInt, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");   
-        console.log("upper case char count "  +  uCaseNumInt  )  
-        console.log("upper password "  + uCasePass);    
-      }
+if (uCaseCheckBox.checked ){
+  tempCalc = uCaseNumInt+ tempCalc;
+}
 
-      if (numCheckBox.checked === true && numInt > 0){
-        numPass = buildPass(numInt, "0123456789"); 
-        console.log("numerical case char count "  +  numInt  ) 
-        console.log("numerical password "  +  numPass); 
-      }
+if (numCheckBox.checked ){
+  tempCalc = numInt+ tempCalc;
+}
 
-      if (specialCheckBox.checked === true && specialInt > 0){
-        specialPass = buildPass(specialInt, " !#$%&'()*+,-./:;<=>?@[]^_`{|}~");
-        console.log("special : CB " + specialCheckBox.checked + "  " +  specialInt  ) 
-        console.log("special password "  + specialPass);  
-      }       
-      
-      
-      unscrmblOutput = "";
-      generatedOutput = "";
-      
-      //Concatenate built string for password for unscrambled output tag
-      unscrmblOutput =  lCasePass + uCasePass  + numPass + specialPass;
+if (specialCheckBox.checked ){
+  tempCalc= specialInt+ tempCalc;
+}
 
-      //Scramble password
+//Add to find total password length
+passLength = tempCalc;
 
-      generatedOutput = shuffle(unscrmblOutput);
+if (passLength >= 8 && passLength <= 128){   
+                        
+                              
+  // Declare blank strings for user char set and the apssword output
+  var generatedOutput= "";
+  var lCasePass = "";
+  var uCasePass = "";
+  var numPass = "";
+  var specialPass = "";
+          
 
-      // Return the password to initGenPassword().
-      return generatedOutput;
+  
+  //Concatenate allowable characters based upon user feedback from confirmation input
+  if (lCaseCheckBox.checked  && lCaseNumInt > 0){
+    lCasePass = buildPass(lCaseNumInt , lCaseCharLib);   
+    console.log("lower case char count "  + lCaseNumInt  )  
+    console.log("lower password "  + lCasePass);             
+  }
 
-      
-    } else {
-        alert("Check correct boxes are checked, values are not zero and password length is between 8 and 128 characters");
-      } 
-  }  
+  if (uCaseCheckBox.checked  && uCaseNumInt > 0  ){
+    uCasePass = buildPass(uCaseNumInt, uCaseCharLib);   
+    console.log("upper case char count "  +  uCaseNumInt  )  
+    console.log("upper password "  + uCasePass);    
+  }
+
+  if (numCheckBox.checked  && numInt > 0){
+    numPass = buildPass(numInt, numCharLib); 
+    console.log("numerical case char count "  +  numInt  ) 
+    console.log("numerical password "  +  numPass); 
+  }
+
+  if (specialCheckBox.checked  && specialInt > 0){
+    specialPass = buildPass(specialInt, specCharLib);
+    console.log("special : CB " + specialCheckBox.checked + "  " +  specialInt  ) 
+    console.log("special password "  + specialPass);  
+  }       
+  
+  
+  unscrmblOutput = "";
+  generatedOutput = "";
+  
+  //Concatenate built string for password for unscrambled output tag
+  unscrmblOutput =  lCasePass + uCasePass  + numPass + specialPass;
+
+  //Scramble password
+
+  generatedOutput = shuffle(unscrmblOutput);
+
+  // Return the password to initGenPassword().
+  return generatedOutput;
+
+  
+} else {
+    alert("Check correct boxes are checked, values are not zero and password length is between 8 and 128 characters");
+  } 
+}  
+
 }
